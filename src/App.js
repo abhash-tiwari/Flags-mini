14bce5b3626a355cf85e0192c3f5afbcb13590a7
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2024-02-23 18:02:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((data) => setCountries(data))
+      .catch((error) => console.error("Error Fetching Data", error));
+  }, []);
+
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    // justifyContent: "space-around",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  const cardStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    border: "2px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    width: "200px",
+  };
+
+  const flagStyle = {
+    height: "100px",
+    width: "100px",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={containerStyle}>
+      {countries.map((ele) => {
+        return (
+          <div key={ele.cca3} style={cardStyle}>
+            <img
+              src={ele.flags.png}
+              alt={`Flag of ${ele.flags.png}`}
+              style={flagStyle}
+            />
+            <h2>{ele.name.common}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
